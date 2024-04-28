c25169ce7be84234a4ae1a57d05769c563197019
Brett M. Morris <bmmorris@stsci.edu>
2024-04-28 14:08:01 -0400

    fix for #4

--- expecto/tests/test_core.py
+++ expecto/tests/test_core.py
@@ -36,3 +36,28 @@ def test_get_spectrum():
         np.log(spectrum.flux[test_mask].value),
         rtol=0.02
     )
+
+
+@pytest.mark.remote_data
+@pytest.mark.parametrize(
+    'params,',
+    [
+        dict(T_eff=3477, log_g=4.833, Z=+0.33, alpha=0),
+        dict(T_eff=3477, log_g=4.833, Z=-0.33, alpha=0.4),
+        dict(T_eff=3477, log_g=4.833, Z=-0.33, alpha=0.8),
+    ]
+)
+def test_metallicity(params):
+    spectrum = get_spectrum(
+        **params
+    )
+
+    # validate that the metadata matches
+    # expectations given the inputs:
+    retrieved_T_eff = spectrum.meta['PHXTEFF']
+    retrieved_Z = spectrum.meta['PHXM_H']
+    retrieved_alpha = spectrum.meta['PHXALPHA']
+
+    assert retrieved_T_eff == np.round(params['T_eff'], -2)
+    assert retrieved_Z == np.round(params['Z'] * 2, 0) / 2
+    assert retrieved_alpha == params['alpha']
